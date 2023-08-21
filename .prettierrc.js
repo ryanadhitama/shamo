4855e4a5dbab94713c267465c703d4eecb25da19
ryanvincere <ryanadhitama2@gmail.com>
2023-08-21 21:52:00 +0800

    hook pre-commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  semi: true,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: "none",
+  printWidth: 100,
+  endOfLine: "auto"
 };
