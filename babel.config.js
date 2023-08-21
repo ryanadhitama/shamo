b402cf28a88b3d3a1a8d9709353c2dc957db9900
ryanvincere <ryanadhitama2@gmail.com>
2023-08-21 22:25:39 +0800

    add svg

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    //if you already have other plugin just paste this lines below
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@assets': './src/assets',
+        }
+      }
+    ]
+  ]
 };
