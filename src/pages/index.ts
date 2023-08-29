c5be8a8254ef2aa607a44b3b678d343a329891ed
ryanadhitama <ryanadhitama2@gmail.com>
2023-08-29 13:07:42 +0800

    init tab

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,7 @@
 export { default as SignIn } from './SignIn';
 export { default as SignUp } from './SignUp';
 export { default as SplashScreen } from './SplashScreen';
+export { default as Home } from './Home';
+export { default as Messages } from './Messages';
+export { default as Favorite } from './Favorite';
+export { default as Profile } from './Profile';
