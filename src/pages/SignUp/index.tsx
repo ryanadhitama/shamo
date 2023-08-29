c1ca547121041304ee20f206888bfb939d0e9e94
ryanvincere <ryanadhitama2@gmail.com>
2023-08-29 21:19:45 +0800

    signup functionality

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,21 +1,60 @@
 import { Email, Lock, Name, Username } from '@assets';
 import { Button, Header, Input } from '@components';
-import { colors, fonts } from '@utils';
+import { colors, fonts, useForm } from '@utils';
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useDispatch } from 'react-redux';
+import { signUpAction } from '../../redux/action';
 
 const SignUp = ({ navigation }: any) => {
+  const [form, setForm] = useForm({
+    name: '',
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  const dispatch = useDispatch();
+
+  const onSubmit = () => {
+    dispatch(signUpAction(form, navigation));
+  };
   return (
     <SafeAreaView style={styles.container}>
       <Header title="Sign Up" desc="Register and Happy Shoping" />
       <View style={styles.content}>
         <View style={styles.form}>
-          <Input prefix={<Name />} label="Full Name" placeholder="Your Full Name" />
-          <Input prefix={<Username />} label="Username" placeholder="Your Username" />
-          <Input prefix={<Email />} label="Email Address" placeholder="Your Email Address" />
-          <Input prefix={<Lock />} secureTextEntry label="Password" placeholder="Your Password" />
-          <Button title="Sign Up" />
+          <Input
+            value={form.name}
+            onChangeText={(value: string) => setForm('name', value)}
+            prefix={<Name />}
+            label="Full Name"
+            placeholder="Your Full Name"
+          />
+          <Input
+            value={form.username}
+            onChangeText={(value: string) => setForm('username', value)}
+            prefix={<Username />}
+            label="Username"
+            placeholder="Your Username"
+          />
+          <Input
+            value={form.email}
+            onChangeText={(value: string) => setForm('email', value)}
+            prefix={<Email />}
+            label="Email Address"
+            placeholder="Your Email Address"
+          />
+          <Input
+            value={form.password}
+            onChangeText={(value: string) => setForm('password', value)}
+            prefix={<Lock />}
+            secureTextEntry
+            label="Password"
+            placeholder="Your Password"
+          />
+          <Button title="Sign Up" onPress={onSubmit} />
         </View>
         <TouchableOpacity onPress={() => navigation.goBack()} style={styles.link}>
           <Text style={styles.linkText}>
--- src/redux/action/auth.ts
+++ src/redux/action/auth.ts
@@ -19,3 +19,20 @@ export const signInAction = (form: any, navigation: any) => (dispatch: any) => {
       showMessage(err?.response?.data?.data?.message || 'Error login', 'danger');
     });
 };
+
+export const signUpAction = (form: any, navigation: any) => (dispatch: any) => {
+  dispatch(setLoading(true));
+  Axios.post(`${API_HOST.url}/register`, form)
+    .then((res: any) => {
+      const token = `${res.data.data.token_type} ${res.data.data.access_token}`;
+      const profile = res.data.data.user;
+      dispatch(setLoading(false));
+      storeData('token', { value: token });
+      storeData('userProfile', profile);
+      navigation.reset({ index: 0, routes: [{ name: 'MainApp' }] });
+    })
+    .catch((err: any) => {
+      dispatch(setLoading(false));
+      showMessage(err?.response?.data?.data?.message || 'Error login', 'danger');
+    });
+};
